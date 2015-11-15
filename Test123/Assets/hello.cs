5127e864d838e3be7ff6429c28ff006d7e4cae6d
test <lunice1@ukr.net>
2015-11-15 21:28:01 +0200

    hello

---
+++ Test123/Assets/hello.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hello : MonoBehaviour {
+	public string max = "Klasnyy";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (max);
+	
+	}
+}
